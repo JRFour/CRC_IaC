71897b279d6ccae05e180deb87f6ecd5d8d55db2
JRFour <j.reale@realewanderer.net>
2024-07-02 14:13:58 +0000

    fixed provider call

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,15 +1,4 @@
 terraform {
-  required_providers {
-    proxmox = {
-      source = "Telmate/proxmox"
-      version = "3.0.1-rc3"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.22.0"
-    }
-  }
-
   backend "s3" {
     bucket = "00terrstate"
     key = "terrraform.tfstate"
