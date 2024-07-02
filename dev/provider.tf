c03eefb81085910d0f893e2fb8b7025e776e85c5
JRFour <j.reale@realewanderer.net>
2024-07-02 16:25:29 +0000

    attempt 9

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,7 +1,7 @@
 provider "proxmox" {
   pm_api_url   = "https://pve.hogwarts.home:8006/api2/json"
   pm_tls_insecure = true
-  pm_api_token_id = var.PM_API_TOKEN_ID
+  pm_api_token_id = var.PM_TOKEN_ID
 }
 
 provider "aws" {
