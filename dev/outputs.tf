bd29868ac233bd77c1b07970bb2c2269f515a504
JRFour <j.reale@realewanderer.net>
2024-07-16 16:05:41 +0000

    created outputs

--- dev/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "container_name_a" {
-  description = "Hostname of container"
-  value       = pve_cfg_a.hostname
-}
-output "container_name_b" {
-  description = "Hostname of container"
-  value       = pve_cfg_b.hostname
-}
