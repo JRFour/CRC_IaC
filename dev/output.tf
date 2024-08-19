8972b4ed0ad5494bbf2fc3a936cac7de6a6ff27a
JRFour <j.reale@realewanderer.net>
2024-08-19 18:25:31 +0000

    corrected outputs

--- dev/output.tf
+++ dev/output.tf
@@ -1,7 +1,11 @@
-output "pve_cfg_a_hostname" {
-  value = module.pve_cfg_a.container_name
-}
+#output "pve_cfg_a_hostname" {
+#  value = module.pve_cfg_a.container_name
+#}
 
-output "pve_cfg_b_hostname" {
-  value = module.pve_cfg_b.container_name
+#output "pve_cfg_b_hostname" {
+#  value = module.pve_cfg_b.container_name
+#}
+
+output "pve_k3s_master_hostname" {
+  value = resource.proxmox_lxc.k3s_master
 }
