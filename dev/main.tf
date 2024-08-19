2e3fcd52ff988f03e3c95fef6d0b13ff06c6054e
JRFour <j.reale@realewanderer.net>
2024-08-19 18:21:35 +0000

    changed for k3s

--- dev/main.tf
+++ dev/main.tf
@@ -1,17 +1,24 @@
-module "pve_cfg_a" {
-  source = "./modules/proxmox_config"
+#module "pve_cfg_a" {
+#  source = "./modules/proxmox_config"
+#
+## Input variables
+#  environment = "dev"
+#  instance = "a"
+#  bridge = "vmbr2"
+#}
 
-# Input variables
-  environment = "dev"
-  instance = "a"
-  bridge = "vmbr2"
-}
+#module "pve_cfg_b" {
+#  source = "./modules/proxmox_config"
+#
+## Input variables
+#  environment = "dev"
+#  instance = "b"
+#  bridge = "vmbr2"
+#}
 
-module "pve_cfg_b" {
-  source = "./modules/proxmox_config"
-
-# Input variables
-  environment = "dev"
-  instance = "b"
-  bridge = "vmbr2"
+resource "proxmox_lxc" "k3s_master" {
+  target_node = "pve"
+  hostname = "k3s_master"
+# ID of container to clone
+  clone = "115"
 }
