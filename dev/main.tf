63af484f8dda0f1a748c99b0ed38a46b402b2a5b
JRFour <j.reale@realewanderer.net>
2024-08-20 14:35:09 +0000

    closing dev

--- dev/main.tf
+++ dev/main.tf
@@ -16,41 +16,15 @@
 #  bridge = "vmbr2"
 #}
 
-resource "proxmox_lxc" "k3s_master" {
-  target_node = "pve"
-  hostname = "k3s-master"
-# ID of container to clone
-  clone = "115"
-  
-  rootfs {
-    storage = "poolboy"
-    size    = "8G"
-  }
-  start = "true"
-}
-
-resource "proxmox_lxc" "k3s_agent_a" {
-  target_node = "pve"
-  hostname = "k3s-agent-a"
-# ID of container to clone
-  clone = "115"
-  
-  rootfs {
-    storage = "poolboy"
-    size    = "8G"
-  }
-  start = "true"
-}
-
-resource "proxmox_lxc" "k3s_agent_b" {
-  target_node = "pve"
-  hostname = "k3s-agent-b"
-# ID of container to clone
-  clone = "115"
-  
-  rootfs {
-    storage = "poolboy"
-    size    = "8G"
-  }
-  start = "true"
-}
+#resource "proxmox_lxc" "k3s_master" {
+#  target_node = "pve"
+#  hostname = "k3s-master"
+## ID of container to clone
+#  clone = "115"
+#  
+#  rootfs {
+#    storage = "poolboy"
+#    size    = "8G"
+#  }
+#  start = "true"
+#}
